0c220271388f4af69880ddd8f1f06133cd4a20fb
Kyle Povey <povey.kyle@gmail.com>
2022-02-20 13:08:15 +0000

    added keyboard and starting guessing mechanic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { randomWordGen } from "../utils/api";
+import Typer from "./Typer";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const [hangWord, setHangWord] = useState("");
+  // const [userGuessArray, setUserGuessArray] =
   const wordOverSix = async () => {
     const word = await randomWordGen();
-    console.log(word, "<<<<<");
     if (word.length >= 6) {
+      return setHangWord(word);
     } else {
       wordOverSix();
     }
   };
-  const test = wordOverSix();
 
-  return <div>{test}</div>;
+  if (hangWord === "") {
+    wordOverSix();
+    if (userGuessArray.length === 0) {
+    }
+  }
+  const splitWord = hangWord.split("");
+  const userGuessArray = splitWord.map((letter) => "_");
+
+  return (
+    <div>
+      <p> {hangWord} </p>
+      <main>
+        <ul>
+          {userGuessArray.map((letter) => {
+            return (
+              <li key={letter} className='letter'>
+                <p> {letter}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+      <Typer
+        input={input}
+        setInput={setInput}
+        layout={layout}
+        setLayout={setLayout}
+      />
+    </div>
+  );
 }
